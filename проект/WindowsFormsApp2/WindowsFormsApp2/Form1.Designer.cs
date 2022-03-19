bb096a25fc2e3089979c36b35959b7c68ae0e879
Munkiru <menzulin505@gmail.com>
2022-03-19 23:00:10 +0600

    выф
    вы

---
+++ проект/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Состояние Двигателя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Состояние Трансмиссии";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Состояние Проводки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Состояние Кузова";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(206, 81);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(206, 109);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(173, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 46);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "План ремонта";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 52);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Узнать цену ремонта";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 53);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "Сохранить чек";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(397, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 16);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "(От 1 до 5)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(397, 59);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 16);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "(От 1 до 5)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(397, 88);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(71, 16);
+            this.label12.TabIndex = 33;
+            this.label12.Text = "(От 1 до 5)";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(397, 115);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(71, 16);
+            this.label13.TabIndex = 34;
+            this.label13.Text = "(От 1 до 5)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 373);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(130, 22);
+            this.richTextBox1.TabIndex = 35;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 16);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Введите цену ремонта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources._470521_Everybody039s_Gone_to_the_Rapture_in_game_CryEngine_Garage_workshop;
+            this.ClientSize = new System.Drawing.Size(989, 565);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ проект/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Workshop repair = new Workshop(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text),Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text));
+            repair.engine = Convert.ToInt32(textBox1.Text);
+            repair.transmission = Convert.ToInt32(textBox2.Text);
+            repair.wiring = Convert.ToInt32(textBox5.Text);
+            repair.body = Convert.ToInt32(textBox6.Text);
+            repair.GetInfo();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                System.IO.File.WriteAllText(filename, richTextBox1.Text);
+            }
+        }
+    }
+
+    class Workshop
+    {
+        public int engine;
+        public int transmission;
+        public int wiring;
+        public int body;
+
+        public Workshop(int engine, int transmission,int wiring, int body)
+        {
+            this.engine = engine;
+            this.transmission = transmission;
+            this.wiring = wiring;
+            this.body = body;
+        }
+        public void GetInfo()
+        {
+           
+            if (engine <= 4 && transmission <=4)
+            {
+                MessageBox.Show($"Требуется ремонт мотора и КПП");
+            }
+            else if (engine <= 4 && wiring <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт мотора и Проводки");
+            }
+            else if (engine <= 4 && body <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт мотора и Кузова");
+            }
+            else if (transmission <= 4 && engine <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт мотора и КПП");
+            }
+            else if (transmission <= 4 && wiring <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Проводки и КПП");
+            }
+            else if (transmission <= 4 && body <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Проводки и КПП");
+            }
+            else if (wiring <= 4 && engine <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Проводки и Мотора");
+            }
+            else if (wiring <= 4 && transmission <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Проводки и КПП");
+            }
+            else if (wiring <= 4 && body <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Проводки и КПП");
+            }
+            else if (body <= 4 && engine <=4)
+            {
+                MessageBox.Show($"Требуется ремонт Кузова и Мотора");
+            }
+            else if (body <= 4 && transmission <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Кузова и КПП");
+            }
+            else if (body <= 4 && wiring <= 4)
+            {
+                MessageBox.Show($"Требуется ремонт Кузова и Проводки");
+            }
+            else if (engine <= 4 && wiring <= 4 && transmission <=4 && body <=4)
+            {
+                MessageBox.Show($"Требуется ремонт Мотора,Проводки,КПП и Кузова");
+            }
+            else if (engine <= 5 && wiring <= 5 && transmission <= 5 && body <= 5)
+            {
+                MessageBox.Show($"Ремонт не Требуется,рекомендуется замена расходников {33500}");
+            }
+        }
+
+    }
+    }
+
+
+
+
